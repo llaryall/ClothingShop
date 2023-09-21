2d8c6ef4330062429ead0aa91593e1b58887736e
llaryall <arya.xll17llx@gmail.com>
2023-09-21 14:34:09 +0330

    chaning the main menu header

--- js/js.js
+++ js/js.js
@@ -1,67 +1,81 @@
 const carousel = document.querySelector(".carousel"),
 firstImg = carousel.querySelectorAll("img")[0],
 arrowIcons = document.querySelectorAll(".wrapper i");
+const menuic=document.getElementById("menu");
+const menu=document.getElementById("menu-bar");
+const categoryic = document.getElementById("cate-ic");
+const categorymenu=document.getElementById('dropdown-menu');
 
-let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+let isDragStart = false,
+  isDragging = false,
+  prevPageX,
+  prevScrollLeft,
+  positionDiff;
 
 const showHideIcons = () => {
-    // showing and hiding prev/next icon according to carousel scroll left value
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
-    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
-}
+  // showing and hiding prev/next icon according to carousel scroll left value
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+  arrowIcons[0].style.display = carousel.scrollLeft === 0 ? "none" : "block";
+  arrowIcons[1].style.display = carousel.scrollLeft === scrollWidth ? "none" : "block";
+};
 
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
-        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
-        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
-    });
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+    // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+    carousel.scrollLeft += icon.id === "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60); // calling showHideIcons2 after 60ms
+  });
 });
 
 const autoSlide = () => {
-    // if there is no image left to scroll then return from here
-    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+  // if there is no image left to scroll then return from here
+  if (carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0)
+    return;
 
-    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
-    let firstImgWidth = firstImg.clientWidth + 14;
-    // getting difference value that needs to add or reduce from carousel left to take middle img center
-    let valDifference = firstImgWidth - positionDiff;
+  positionDiff = Math.abs(positionDiff); // making positionDiff2 value positive
+  let firstImgWidth = firstImg.clientWidth + 14;
+  // getting difference value that needs to add or reduce from carousel2 left to take the middle img center
+  let valDifference = firstImgWidth - positionDiff;
 
-    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
-        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-    }
-    // if user is scrolling to the left
-    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-}
+  if (carousel.scrollLeft > prevScrollLeft) {
+    // if the user is scrolling to the right
+    return (carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff);
+  }
+
+  // if the user is scrolling to the left
+  carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+};
 
 const dragStart = (e) => {
-    // updatating global variables value on mouse down event
-    isDragStart = true;
-    prevPageX = e.pageX || e.touches[0].pageX;
-    prevScrollLeft = carousel.scrollLeft;
-}
+  // updating global variables value on mouse down event
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+  e.preventDefault();
+};
 
 const dragging = (e) => {
-    // scrolling images/carousel to left according to mouse pointer
-    if(!isDragStart) return;
-    e.preventDefault();
-    isDragging = true;
-    carousel.classList.add("dragging");
-    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-    showHideIcons();
-}
+  // scrolling images/carousel to the left according to the mouse pointer
+  if (!isDragStart) return;
+  e.preventDefault();
+  isDragging = true;
+  carousel.classList.add("dragging");
+  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHideIcons();
+};
 
 const dragStop = () => {
-    isDragStart = false;
-    carousel.classList.remove("dragging");
+  isDragStart = false;
+  carousel.classList.remove("dragging");
 
-    if(!isDragging) return;
-    isDragging = false;
-    autoSlide();
-}
+  if (!isDragging) return;
+  isDragging = false;
+  autoSlide();
+};
+
+
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("touchstart", dragStart);
@@ -71,6 +85,10 @@ carousel.addEventListener("touchmove", dragging);
 
 document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("touchend", dragStop);
+
+
+
+
 
 
 
@@ -247,8 +265,26 @@ carousel3.addEventListener("touchend", dragStop3);
 
 
 
+function openmenu() {
+  if (menu.classList.contains('show-menu')) {
+    menu.classList.remove('show-menu');
+  } else {
+    menu.classList.add('show-menu');
+  }
+}
+menuic.addEventListener('click', openmenu);
 
 
-
-
+function toggleCategoryMenu() {
+  if (categorymenu.style.height === '0px' || categorymenu.style.height === '') {
+    categorymenu.style.height = "200px";
+    categorymenu.style.opacity = '1';
+  } else {
+    categorymenu.style.height = "0px";
+    setTimeout(function () {
+      categorymenu.style.opacity = '0';
+    }, 200);
+  }
+}
+categoryic.addEventListener('click', toggleCategoryMenu);
 
